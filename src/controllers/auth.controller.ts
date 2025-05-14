efba80a01f416f330eecf38e5800c1f80e07fee9
kemalcrisannaufal <kemalcrisannnaufal@gmail.com>
2025-05-14 18:42:14 +0700

    feat: add auth (register, login, me) api

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,130 @@
+import { Request, Response } from "express";
+import * as Yup from "yup";
+import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
+import { generateToken } from "../utils/jwt";
+import { IReqUser } from "../middlewares/auth.middleware";
+
+type TRegister = {
+  fullname: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+type TLogin = {
+  identifier: string;
+  password: string;
+};
+
+const registerValidateSchema = Yup.object({
+  fullname: Yup.string().required(),
+  username: Yup.string().required(),
+  email: Yup.string().required(),
+  password: Yup.string().required(),
+  confirmPassword: Yup.string()
+    .required()
+    .oneOf([Yup.ref("password"), ""], "Password doesn't match"),
+});
+
+const loginValidateSchema = Yup.object({
+  identifier: Yup.string().required(),
+  password: Yup.string().required(),
+});
+
+export default {
+  async register(req: Request, res: Response) {
+    const { fullname, username, email, password, confirmPassword } =
+      req.body as unknown as TRegister;
+
+    try {
+      await registerValidateSchema.validate({
+        fullname,
+        username,
+        email,
+        password,
+        confirmPassword,
+      });
+
+      const result = await UserModel.create({
+        fullname,
+        username,
+        email,
+        password,
+      });
+
+      res.status(200).json({
+        message: "Success Registration",
+        data: result,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({ message: err.message, data: null });
+    }
+  },
+
+  async login(req: Request, res: Response) {
+    const { identifier, password } = req.body as unknown as TLogin;
+    try {
+      await loginValidateSchema.validate({ identifier, password });
+
+      // ambil data user berdasarkan "identifier" -> email dan username
+      const userByIdentifier = await UserModel.findOne({
+        $or: [
+          {
+            email: identifier,
+          },
+          {
+            username: identifier,
+          },
+        ],
+      });
+
+      // Validasi password input dengan password di database
+      if (!userByIdentifier) {
+        return res.status(403).json({
+          message: "user not found",
+          data: null,
+        });
+      }
+
+      const validatePassword: boolean =
+        encrypt(password) === userByIdentifier.password;
+
+      if (!validatePassword) {
+        return res.status(403).json({
+          message: "Invalid Password",
+          data: null,
+        });
+      }
+
+      const token = generateToken({
+        id: userByIdentifier._id,
+        role: userByIdentifier.role,
+      });
+
+      res.status(200).json({
+        message: "Success Login",
+        data: token,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({ message: err.message, data: null });
+    }
+  },
+
+  async me(req: IReqUser, res: Response) {
+    try {
+      const user = req.user;
+      const result = await UserModel.findById(user?.id);
+      res.status(200).json({
+        message: "Success Get User Profile",
+        data: result,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({ message: err.message, data: null });
+    }
+  },
+};
---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import { getUserData, IUserToken } from "../utils/jwt";
+
+export interface IReqUser extends Request {
+  user?: IUserToken;
+}
+
+export default (req: Request, res: Response, next: NextFunction) => {
+  const authorization = req.headers?.authorization;
+
+  if (!authorization) {
+    return res.status(403).json({ message: "Unauthorized", data: null });
+  }
+
+  const [prefix, token] = authorization.split(" ");
+
+  if (!(prefix === "Bearer" && token)) {
+    return res.status(403).json({ message: "Unauthorized", data: null });
+  }
+
+  let user;
+  try {
+    user = getUserData(token);
+  } catch (error) {
+    return res.status(403).json({ message: "Invalid token", data: null });
+  }
+
+  if (!user) {
+    return res.status(403).json({ message: "Unauthorized", data: null });
+  }
+
+  (req as IReqUser).user = user;
+  next();
+};
